9b10fe61ee248cd995d3e9e1a12b1565db5e1bf9
stevemurphy256 <smurphy5884@icloud.com>
2017-11-17 15:14:18 -0500

    component state check!

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-
 import Header from './Header';
 
-const App = () => {
-    return(
-        <div className= "App">
-            <Header message= "Naming Contests" />
-            <div>
-                boing
+class App extends React.Component {
+    state = {
+        pageHeader: 'Naming Contests are fun!'
+    };
+    render() {
+        return(
+            <div className= "App">
+                <Header message= {this.state.pageHeader} />
+                <div>
+                    ----
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default App;
